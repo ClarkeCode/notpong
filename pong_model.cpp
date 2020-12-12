3fbe53aa1e2b0c32b2cc9ddd4068c2eded152a67
Robert Clarke <clarke.john.robert@gmail.com>
2020-12-12 08:13:20 -0500

    First pass at a game model for pong

---
+++ pong_model.cpp
@@ -0,0 +1,30 @@
+class Paddle {
+    double xpos, ypos;
+    double width;
+    double speed;
+    
+    public:
+    Paddle(double x, double y, double w, double s) : xpos(x), ypos(y), width(w), speed(s) {}
+    Paddle() : Paddle(0, 0, 0, 0) {}
+};
+
+class Ball {
+    double xpos, ypos;
+    double radius;
+    double speed;
+    double direction;
+
+    public:
+    Ball(double x, double y, double r, double s, double d) : xpos(x), ypos(y), radius(r), speed(s), direction(d) {}
+    Ball() : Ball(0, 0, 0, 0, 0) {}
+};
+
+class PongModel {
+    Paddle P1Paddle, P2Paddle;
+    Ball PongBall;
+    double MaxPaddleLocation, MinPaddleLocation;
+
+    public:
+    PongModel() {}
+    ~PongModel() {}
+};
