ae13b0b6ab79d1f2da09a9a0c2b6ea0a278bede0
Robert Clarke <clarke.john.robert@gmail.com>
2020-12-18 22:20:20 -0500

    First pass at PongModel constructor

--- src/pong_model.cpp
+++ src/pong_model.cpp
@@ -1,5 +1,17 @@
 #include "pong_model.hpp"
 
 pong::PongModel::PongModel(GameWorld& gw) {
+    topBottomWallThickness = 10;
+    MaxPaddleYPosition = 0 + topBottomWallThickness;
+    MinPaddleYPosition = gw.height - topBottomWallThickness;
     
+    paddleWidth = 20;
+    paddleHeight = 60;
+    paddleSpeed = 10;
+    P1Paddle = Paddle(0 + paddleWidth/2.0, gw.height/2.0, paddleWidth, paddleHeight, paddleSpeed);
+    P2Paddle = Paddle(gw.width - paddleWidth/2.0, gw.height/2.0, paddleWidth, paddleHeight, paddleSpeed);
+
+    PongBall.xpos = gw.width/2.0;
+    PongBall.ypos = gw.height/2.0;
+    PongBall.speed = 20;
 }
--- src/pong_model.hpp
+++ src/pong_model.hpp
@@ -7,33 +7,41 @@ namespace pong {
         GameWorld(int horizontalWidth, int verticalHeight) : width(horizontalWidth), height(verticalHeight) {}
     };
 
+    class GameObject {
+        public:
+        virtual void drawObject() = delete;
+        virtual ~GameObject() {}
+    };
+
     class Paddle {
+        public:
         double xpos, ypos;
-        double width;
+        double width, length;
         double speed;
         
-        public:
-        Paddle(double x, double y, double w, double s) : xpos(x), ypos(y), width(w), speed(s) {}
-        Paddle() : Paddle(0, 0, 0, 0) {}
+        Paddle(double x, double y, double w, double l, double s) : xpos(x), ypos(y), width(w), length(l), speed(s) {}
+        Paddle() : Paddle(0, 0, 0, 0, 0) {}
     };
 
     class Ball {
+        public:
         double xpos, ypos;
         double radius;
         double speed;
         double direction;
 
-        public:
         Ball(double x, double y, double r, double s, double d) : xpos(x), ypos(y), radius(r), speed(s), direction(d) {}
         Ball() : Ball(0, 0, 0, 0, 0) {}
     };
 
     class PongModel {
+        public:
         Paddle P1Paddle, P2Paddle;
         Ball PongBall;
-        double MaxPaddleLocation, MinPaddleLocation;
+        int topBottomWallThickness;
+        double paddleWidth, paddleHeight, paddleSpeed;
+        double MaxPaddleYPosition, MinPaddleYPosition;
 
-        public:
         PongModel() {}
         PongModel(GameWorld& gw);
         ~PongModel() {}
