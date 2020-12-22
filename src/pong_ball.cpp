39453576d50efa5bad60cc6edac8d9101324dd6b
Robert Clarke <clarke.john.robert@gmail.com>
2020-12-22 02:35:18 -0500

    Finished implementation of bouncing off all walls and paddles, removed unused helper functions

--- src/pong_ball.cpp
+++ src/pong_ball.cpp
@@ -2,20 +2,6 @@
 #include "raylib.h"
 #include "raymath.h"
 
-//UTIL
-//========
-// Normalize input value within input range
-RMDEF float Normalize(float value, float start, float end) {
-    return (value - start) / (end - start);
-}
-
-// Remap input value within input range to output range
-RMDEF float Remap(float value, float inputStart, float inputEnd, float outputStart, float outputEnd) {
-    return (value - inputStart) / (inputEnd - inputStart) * (outputEnd - outputStart) + outputStart;
-}
-//========
-
-
 pong::Ball::Ball(float x, float y, float r, float s, float d)  : GeometryColours(Color{0,0,230,255}, Color{60,60,60,255}), xyPosition(Vector2{x,y}), radius(r), speed(s) {
     setDirection(d);
 }
@@ -41,48 +27,13 @@ Vector2 getRectangleCoord(Rectangle const& rect, RectangleCorner desiredCorner) 
 }
 
 bool isValueWithinRange(float value, float min, float max) { return value >= min && value <= max; }
-
-Vector2 haltTravelAtXBarrier(Vector2& coord, Vector2& target, float barrier) {
-    return Vector2 {barrier, Remap(target.y, coord.y, target.y, coord.x, barrier)};
-}
-Vector2 haltTravelAtYBarrier(Vector2& coord, Vector2& target, float barrier) {
-    return Vector2 {Remap(target.x, coord.x, target.x, 5,7), barrier};
-}
-
-float flipAngleVerticalCollision(float angle) {
-    if (angle < PI/2.0) //Q1 -> Q2
-        return PI - angle;
-    else if (angle < PI) //Q2 -> Q1
-        return PI - angle;
-    else if (angle < 3*PI/2.0) //Q3 -> Q4  3PI-angle
-        return 2*PI - (angle - PI);
-    else //Q4 -> Q3
-        return PI + (2*PI - angle);
-}
-
-float flipAngleHorizontalCollision(float angle) {
-    if (angle < PI/2.0) //Q1 -> Q4
-        return 2*PI - angle;
-    else if (angle < PI) //Q2 -> Q3
-        return PI + (PI - angle);
-    else if (angle < 3*PI/2.0) //Q3 -> Q2
-        return PI - (angle-PI);
-    else //Q4 -> Q1
-        return 2*PI - angle;
-}
-
-bool isPointWithinYValues(Vector2& xyCoord, Rectangle& rect) {
+bool isPointWithinYValues(Vector2 const& xyCoord, Rectangle const& rect) {
     return xyCoord.y >= rect.y && xyCoord.y <= rect.y + rect.height;
 }
-bool isPointWithinXValues(Vector2& xyCoord, Rectangle& rect) {
+bool isPointWithinXValues(Vector2 const& xyCoord, Rectangle const& rect) {
     return xyCoord.x >= rect.x && xyCoord.x <= rect.x + rect.width;
 }
-bool isPointBelowYValue(Vector2& xyCoord, float yValue) {
-    return xyCoord.y > yValue; //Larger Y-values are lower in screen-space
-}
-bool isPointLeftOfXValue(Vector2& xyCoord, float xValue) {
-    return xyCoord.x < xValue;
-}
+
 void pong::Ball::updateBall(float frameTime, PongModel& gameModel) {
     //If the game has not started, do not move
     if (!gameModel.canBallMove) {
@@ -98,7 +49,35 @@ void pong::Ball::updateBall(float frameTime, PongModel& gameModel) {
 
     Vector2 targetCoord = Vector2Add(xyPosition, scaledMovementVect);
     float unusedFrametimeRatio = 0;
-    if (isValueWithinRange(BottomWallCollisionZone.y, xyPosition.y, targetCoord.y)) { //Will bounce
+
+    //Bouncing off paddles
+    if (isValueWithinRange(P1PaddleCollisionZone.x+P1PaddleCollisionZone.width, targetCoord.x, xyPosition.x)) { //Ball will pass by the right side of the paddle
+        unusedFrametimeRatio = (xyPosition.x-(P1PaddleCollisionZone.x+P1PaddleCollisionZone.width))/(xyPosition.x-targetCoord.x);
+        Vector2 candidatePosition = Vector2Add(xyPosition, Vector2Scale(scaledMovementVect, unusedFrametimeRatio));
+        if (isPointWithinYValues(candidatePosition, P1PaddleCollisionZone)) { //Ball will be in contact as it goes to the right side of the paddle
+            xyPosition = candidatePosition;
+            directionVect.x *= -1; //Invert x movement
+            updateBall((1-unusedFrametimeRatio)*frameTime, gameModel);
+        }
+    }
+    else if (isValueWithinRange(P2PaddleCollisionZone.x, xyPosition.x, targetCoord.x)) { //Ball will pass by the left side of the paddle
+        unusedFrametimeRatio = (P2PaddleCollisionZone.x-xyPosition.x)/(targetCoord.x-xyPosition.x);
+        Vector2 candidatePosition = Vector2Add(xyPosition, Vector2Scale(scaledMovementVect, unusedFrametimeRatio));
+        if (isPointWithinYValues(candidatePosition, P2PaddleCollisionZone)) { //Ball will be in contact as it goes to the left side of the paddle
+            xyPosition = candidatePosition;
+            directionVect.x *= -1; //Invert x movement
+            updateBall((1-unusedFrametimeRatio)*frameTime, gameModel);
+        }
+    }
+
+    //Bouncing off walls
+    if (isValueWithinRange(TopWallCollisionZone.y+TopWallCollisionZone.height, targetCoord.y, xyPosition.y)) {
+        unusedFrametimeRatio = (xyPosition.y-(TopWallCollisionZone.y+TopWallCollisionZone.height))/(xyPosition.y-targetCoord.y);
+        xyPosition = Vector2Add(xyPosition, Vector2Scale(scaledMovementVect, unusedFrametimeRatio));
+        directionVect.y *= -1; //Invert y movement
+        updateBall((1-unusedFrametimeRatio)*frameTime, gameModel);
+    }
+    else if (isValueWithinRange(BottomWallCollisionZone.y, xyPosition.y, targetCoord.y)) { //Will bounce
         unusedFrametimeRatio = (BottomWallCollisionZone.y-xyPosition.y)/(targetCoord.y-xyPosition.y);
         xyPosition = Vector2Add(xyPosition, Vector2Scale(scaledMovementVect, unusedFrametimeRatio));
         directionVect.y *= -1; //Invert y movement
