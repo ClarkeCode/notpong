d5144f533e259178252881752bb2b55924b3bfe2
Robert Clarke <clarke.john.robert@gmail.com>
2020-12-23 14:21:22 -0500

    More Rectangle helpers, inlining header functions

--- src/raylib_extensions.hpp
+++ src/raylib_extensions.hpp
@@ -7,11 +7,17 @@ namespace extensions {
     //Rectangle related extensions
     enum RectangleSide { TOP, BOTTOM, LEFT, RIGHT };
     enum RectangleVertex { TOPLEFT, TOPRIGHT, BOTTOMLEFT, BOTTOMRIGHT };
-    
-    Rectangle RectangleExpandByAmount(Rectangle const& rect, float amount) {
+
+    inline Rectangle RectangleCentreOnCoord(Vector2 const& coord, float width, float height) {
+        return Rectangle{coord.x-width/2.0f, coord.y-height/2.0f, width, height};
+    }
+    inline Rectangle RectangleCentreOnCoord(Vector2 const& coord, Rectangle const& rect) {
+        return Rectangle{coord.x-rect.width/2.0f, coord.y-rect.height/2.0f, rect.width, rect.height};
+    }
+    inline Rectangle RectangleExpandByAmount(Rectangle const& rect, float amount) {
         return Rectangle{rect.x-amount, rect.y-amount, rect.width+2*amount, rect.height+2*amount};
     }
-    Vector2 RectangleGetVertexCoord(Rectangle const& rect, RectangleVertex const& desiredVertex) {
+    inline Vector2 RectangleGetVertexCoord(Rectangle const& rect, RectangleVertex const& desiredVertex) {
         if (desiredVertex == TOPLEFT)
             return Vector2{rect.x, rect.y};
         else if (desiredVertex == TOPRIGHT)
@@ -22,11 +28,11 @@ namespace extensions {
             return Vector2{rect.x+rect.width, rect.y+rect.height};
     }
 
-    bool isValueWithinRange(float value, float min, float max) { return value >= min && value <= max; }
-    bool isPointWithinYValues(Vector2 const& xyCoord, Rectangle const& rect) {
+    inline bool isValueWithinRange(float value, float min, float max) { return value >= min && value <= max; }
+    inline bool isPointWithinYValues(Vector2 const& xyCoord, Rectangle const& rect) {
         return xyCoord.y >= rect.y && xyCoord.y <= rect.y + rect.height;
     }
-    bool isPointWithinXValues(Vector2 const& xyCoord, Rectangle const& rect) {
+    inline bool isPointWithinXValues(Vector2 const& xyCoord, Rectangle const& rect) {
         return xyCoord.x >= rect.x && xyCoord.x <= rect.x + rect.width;
     }
 }
